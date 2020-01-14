fbf968a3461ee94bc45f2b9ffa9dd6c4e7a2d328
elmaester <olegmorket@gmail.com>
2020-01-14 12:48:43 +0200

    fix: resized photo to be smaller

--- components/Navigation.js
+++ components/Navigation.js
@@ -40,10 +40,10 @@ const Nav = styled.nav`
         pointer-events: none;
         @media ${sizes.desktop} {
           pointer-events: auto;
-          border-left: 5px solid ${colors.darkBlue};
+          /* border-left: 5px solid ${colors.darkBlue};
           border-right: 5px solid ${colors.darkBlue};
           border-radius: 35%;
-          padding: 3px 10px;
+          padding: 3px 10px; */
         }
       }
     }
